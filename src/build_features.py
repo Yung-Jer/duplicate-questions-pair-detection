e3c1553e501f5d4fe21180350350207b86e53c90
Calven Ng <calvenng.97@gmail.com>
2022-10-05 23:21:38 +0800

    Create build_features.py

---
+++ src/build_features.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  5 23:17:27 2022
+
+@author: Calven Ng, Tay Xun Yang, Wong Yung Jer, Cheang Xue Ting, Tiara Lau
+"""
+
+import pandas as pd
+
+
+train_df = pd.read_csv('../data/raw/train.csv')
