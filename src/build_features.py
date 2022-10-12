204d6bd219571aea5da24019e5c10adc5c932c14
Calven Ng <calvenng.97@gmail.com>
2022-10-12 20:37:14 +0800

    Tidy up

--- src/build_features.py
+++ src/build_features.py
@@ -4,81 +4,12 @@ Created on Wed Oct  5 23:17:27 2022
 
 @author: Calven Ng, Tay Xun Yang, Wong Yung Jer, Cheang Xue Ting, Tiara Lau
 """
-# python3 -m spacy download enimport numpy as np
 import pandas as pd
+import topic_modeling as tm
+import build_startingword_lcs as sw
 
-import os
-from nltk.corpus import stopwords
-import re, nltk, spacy, string
-
-from sklearn.decomposition import LatentDirichletAllocation
-from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from pprint import pprint
-import numpy as np
-
-
-abspath = os.path.abspath(__file__)
-dname = os.path.dirname(abspath)
-os.chdir(dname)
-
-TOTAL_TOPICS = 40
-train_df_raw = pd.read_csv('../data/raw/train.csv')
-train_df = train_df_raw.dropna()
+train_df = pd.read_feather('../data/processed/train_clean.feather')
 
 
 
-
-def clean_text(text):
-    '''Make text lowercase, remove text in square brackets, remove punctuation and remove words containing numbers.'''
-    text = text.lower()
-    text = re.sub(r'\[.*?\]', '', text)
-    text = re.sub(r'[%s]' % re.escape(string.punctuation), '', text)
-    text = re.sub(r'\w*\d\w*', '', text)
-    return text
-
-df = pd.DataFrame(train_df.question1.apply(lambda x: clean_text(x)))
-
-nlp = spacy.load('en_core_web_sm')
-def lemmatizer(text):        
-    sent = []
-    doc = nlp(text)
-    for word in doc:
-        sent.append(word.lemma_)
-    return " ".join(sent)
-    
-df["question_lemmatize"] =  df.apply(lambda x: lemmatizer(x['question1']), axis=1)
-df['question_lemmatize_clean'] = df['question_lemmatize'].str.replace('-PRON-', '')
-
-
-vectorizer = CountVectorizer(analyzer='word',       
-                             min_df=3,                       
-                             stop_words='english',             
-                             lowercase=True,                   
-                             token_pattern='[a-zA-Z0-9]{3,}',  
-                             max_features=5000,          
-                            )
-
-data_vectorized = vectorizer.fit_transform(df['question_lemmatize_clean'])
-
-lda_model = LatentDirichletAllocation(n_components=30, # Number of topics
-                                      learning_method='online',
-                                      random_state=0,       
-                                      n_jobs = -1  # Use all available CPUs
-                                     )
-lda_output = lda_model.fit_transform(data_vectorized)
-
-def show_topics(vectorizer=vectorizer, lda_model=lda_model, n_words=2):
-    keywords = np.array(vectorizer.get_feature_names())
-    topic_keywords = []
-    for topic_weights in lda_model.components_:
-        top_keyword_locs = (-topic_weights).argsort()[:n_words]
-        topic_keywords.append(keywords.take(top_keyword_locs))
-    return topic_keywords
-
-topic_keywords = show_topics(vectorizer=vectorizer, lda_model=lda_model, n_words=2)
-
-df_topic_keywords = pd.DataFrame(topic_keywords)
-df_topic_keywords.columns = ['Word '+str(i) for i in range(df_topic_keywords.shape[1])]
-df_topic_keywords.index = ['Topic '+str(i) for i in range(df_topic_keywords.shape[0])]
-df_topic_keywords
 
